8ed7c2d1619224ab509d2dfe3fbb5fbddc32c6e1
Breno Silva Menezes <bmenezes_2011@hotmail.com>
2026-07-29 17:20:05 -0300

    feat: add console navigation

--- M365-Admin-Console.ps1
+++ M365-Admin-Console.ps1
@@ -4,7 +4,7 @@ Import-Module "$PSScriptRoot\Modules\Core.psm1" -Force
 # Carrega os arquivos da interface
 . "$PSScriptRoot\UI\Banner.ps1"
 . "$PSScriptRoot\UI\Menu.ps1"
+. "$PSScriptRoot\Console\Console.ps1"
 
 # Exibe a interface
-Show-Banner
-Show-Menu
+Start-Console
