bf799743f921a8b7262a04cf1de7069ebc049e89
Breno Silva Menezes <bmenezes_2011@hotmail.com>
2026-08-21 16:36:14 -0300

    feat(exchange-auth): implementa conexao e contexto do Exchange Online

--- M365-Admin-Console.ps1
+++ M365-Admin-Console.ps1
@@ -1,6 +1,7 @@
 # Importa o módulo Core
 Import-Module "$PSScriptRoot\Modules\Core.psm1" -Force
 Import-Module "$PSScriptRoot\Modules\Graph.psm1" -Force
+Import-Module "$PSScriptRoot\Modules\Exchange.psm1" -Force
 
 # Carrega os arquivos da interface
 . "$PSScriptRoot\UI\Banner.ps1"
